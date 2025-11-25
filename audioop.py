2ccc45d7edc92998dee4627b3ebbc09c2ead5b4e
HK <ez.angot@gmail.com>
2025-11-25 16:19:34 +0100

    Delete audioop.py

--- audioop.py
+++
@@ -1,9 +0,0 @@
-# Dummy audioop module for Python 3.13
-def add(a, b, width): return b
-def mul(a, factor, width): return a
-def max(a, width): return 0
-def avg(a, width): return 0
-def rms(a, width): return 0
-
-def ratecv(fragment, width, channels, inrate, outrate, state, weightA=1, weightB=0):
-    return fragment, state
